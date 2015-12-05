bc1e025fb5e53fb9acb32ab61a567cedf5ae791c
Nick Pfundstein <nickpfundstein@gmail.com>
2015-12-05 11:50:31 -0500

    5-1 completed

---
+++ 5-1.php
@@ -0,0 +1,15 @@
+<?php
+
+$strings = file('5.txt', FILE_IGNORE_NEW_LINES);
+
+$nice = 0;
+
+foreach ($strings as $string) {
+	if (preg_match('/[aeiou].*[aeiou].*[aeiou]/', $string)
+	    && preg_match('/(.)\1/', $string)
+	    && !preg_match('/ab|cd|pq|xy/', $string)) {
+		$nice += 1;
+	}
+}
+
+var_dump($nice);
