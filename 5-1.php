ee3a60d63cd9c62e4189e5ad06c82ebf2f5763ee
Nick Pfundstein <nickpfundstein@gmail.com>
2015-12-07 11:53:57 -0500

    Reformat days 5 and 6

--- 5-1.php
+++ 5-1.php
@@ -5,11 +5,12 @@ $strings = file('5.txt', FILE_IGNORE_NEW_LINES);
 $nice = 0;
 
 foreach ($strings as $string) {
-	if (preg_match('/[aeiou].*[aeiou].*[aeiou]/', $string)
-	    && preg_match('/(.)\1/', $string)
-	    && !preg_match('/ab|cd|pq|xy/', $string)) {
-		$nice += 1;
-	}
+    if (preg_match('/[aeiou].*[aeiou].*[aeiou]/', $string)
+        && preg_match('/(.)\1/', $string)
+        && !preg_match('/ab|cd|pq|xy/', $string)
+    ) {
+        $nice += 1;
+    }
 }
 
 var_dump($nice);
--- 5-2.php
+++ 5-2.php
@@ -5,10 +5,11 @@ $strings = file('5.txt', FILE_IGNORE_NEW_LINES);
 $nice = 0;
 
 foreach ($strings as $string) {
-	if (preg_match('/(..).*\1/', $string)
-	    && preg_match('/(.).\1/', $string)) {
-		$nice += 1;
-	}
+    if (preg_match('/(..).*\1/', $string)
+        && preg_match('/(.).\1/', $string)
+    ) {
+        $nice += 1;
+    }
 }
 
 var_dump($nice);
