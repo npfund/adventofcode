5c7ec498eb3d03a8c0fd1dc6503fb67ba3ecb284
Nick Pfundstein <nickpfundstein@gmail.com>
2015-12-08 09:40:17 -0500

    8-2 completed

---
+++ 8/8-2.php
@@ -0,0 +1,14 @@
+<?php
+
+$strings = file('8.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$raw = 0;
+$slashed = 0;
+
+foreach ($strings as $string) {
+    $raw += strlen($string);
+    $slashed += strlen(addslashes($string));
+    var_dump(addslashes($string));
+}
+
+var_dump(($slashed - $raw) + (2 * count($strings)));
