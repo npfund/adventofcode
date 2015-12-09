892e6e22609c5b11ca0ce01ea96e0a43dc4656af
Nick Pfundstein <nickpfundstein@gmail.com>
2015-12-09 12:10:53 -0500

    9-1 completed

---
+++ 9/9-1.php
@@ -0,0 +1,42 @@
+<?php
+
+include 'vendor/autoload.php';
+
+$input = file('9.txt', FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+
+$distances = [];
+
+foreach ($input as $line) {
+    list($lhs, $distance) = explode(' = ', $line);
+    list($start, $end) = explode (' to ', $lhs);
+
+    if (!isset($distances[$start])) {
+        $distances[$start] = [];
+    }
+
+    $distances[$start][$end] = $distance;
+
+    if (!isset($distances[$end])) {
+        $distances[$end] = [];
+    }
+
+    $distances[$end][$start] = $distance;
+}
+
+$permutations = new \NajiDev\Permutation\PermutationIterator(array_keys($distances));
+
+$shortest = PHP_INT_MAX;
+
+foreach ($permutations as $cities) {
+    $total = 0;
+
+    for ($i = 0; $i < count($cities) - 1; ++$i) {
+        $total += $distances[$cities[$i]][$cities[$i + 1]];
+    }
+
+    if ($total < $shortest) {
+        $shortest = $total;
+    }
+}
+
+var_dump($shortest);
